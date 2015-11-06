d84e22010a7f01c2853e4c98a10e1de464e9e028
cxtadment <cxtadam@gmail.com>
2015-11-06 12:28:32 -0800

    all most finish it

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_object("config")
+app.secret_key = '\xf0&#\xf2\xdb\xf2\x0b9\x10\x147\xce\xb2\xc2\x7fB\x13A>\xfdG\xaa\xca\xd3'
 
 db = MongoEngine(app)
 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from flask.ext.mongoengine.wtf import model_form
 from app import db
 import datetime
 
@@ -6,3 +7,5 @@ class Todo(db.Document):
     content = db.StringField(required=True, max_length=20)
     time = db.DateTimeField(default=datetime.datetime.now())
     status = db.IntField(default=0)
+
+TodoForm = model_form(Todo)

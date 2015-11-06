e2129bf1d59fae94c08feaafb6adcd0c1538aa85
cxtadment <cxtadam@gmail.com>
2015-11-06 01:11:27 -0800

    add save function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from app.models import Todo
+
 
 @app.route('/')
 def index():
-    return render_template("index.html", text="Hello World")
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
+
+@app.route('/add', methods=['POST', ])
+def add():
+    content = request.form.get("content")
+    todo = Todo(content=content)
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
